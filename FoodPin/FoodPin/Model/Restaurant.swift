e20a4f180b0ba6cfe906d245fdb94f374b47b639
Andreea Sburlea <andreea.sburlea@fortech.ro>
2022-04-19 12:17:15 +0300

    Chapter 11: OOP structures and classes

---
+++ FoodPin/FoodPin/Model/Restaurant.swift
@@ -0,0 +1,66 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by Andreea Sburlea on 4/19/22.
+//
+
+ struct Restaurant: Hashable {
+    private var _name: String = ""
+     var name: String {
+         get {
+             return _name
+         }
+         set {
+             _name = newValue
+         }
+     }
+
+    private var _type: String = ""
+     var type: String {
+         get {
+             return _type
+         }
+         set {
+             _type = newValue
+         }
+     }
+
+    private var _location: String = ""
+     var location: String {
+         get {
+             return _location
+         }
+         set {
+             _location = newValue
+         }
+     }
+
+    private var _image: String = ""
+     var image: String {
+         get {
+             return _image
+         }
+         set {
+             _image = newValue
+         }
+     }
+     
+    private var _isFavorite: Bool = false
+     var isFavorite: Bool {
+         get {
+             return _isFavorite
+         }
+         set {
+             _isFavorite = newValue
+         }
+     }
+
+    public init(name: String, type: String, location: String, image: String, isFavorite: Bool) {
+        self._name = name
+        self._type = type
+        self._location = location
+        self._image = image
+        self._isFavorite = isFavorite
+    }
+}
